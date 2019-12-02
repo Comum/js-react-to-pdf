d147d9b7ff8184f5e738ff8062d666d4631946d0
Miguel Ribeiro <miguelribeiro@MacBook-Air.home>
2019-12-02 21:37:16 +0000

    chore: add title component

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import Title from '../Title/Title';
+
 import './MainMenu.css';
 
 function MainMenu() {
-	return <div className="main-menu">I am the main menu</div>;
+	return (
+		<div className="main-menu">
+			<Title title="Miguel Ribeiro" />	
+		</div>
+	);
 }
 
 export default MainMenu;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Title.css';
+
+function Title(props) {
+    const { title } = props;
+
+return (<div class="title">{title}</div>)
+}
+
+export default Title;
